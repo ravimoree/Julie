d1acb8b0051bb5d372670d85b35fe68173f25824
ravimoree <ravi@diinfotech.com>
2024-01-16 11:50:26 +0530

    Add Employee

---
+++ JulieInventoryMVC/JulieInventoryMVC/Controllers/ItemWiseRateChartController.cs
@@ -0,0 +1,31 @@
+﻿using JulieInventoryMVC.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JulieInventoryMVC.Controllers
+{
+    [SessionExpire(true)]
+    public class ItemWiseRateChartController : Controller
+    {
+        // GET: ItemWiseRateChart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
